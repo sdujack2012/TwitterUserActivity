1a61e318018e40b2824c86e525e1bcb049387e77
Kai_Jiang <Kai_Jiang@RWCP.livingstone.int>
2015-06-16 15:58:54 +1000

    main class of this application to be implement

---
+++ TwitterUserActivity.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TwitterUserActivity
+ *
+ * @author Kai_Jiang
+ */
+class TwitterUserActivity {
+    //put your code here
+}
